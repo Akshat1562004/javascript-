da1a4513cd43637706971d5bf3384c56d7913415
Akshat1562004 <akshat.shubhit15@gmail.com>
2025-02-17 15:48:04 +0000

    variables

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,8 @@
+const accountId=14453
+let accountEmail="hitesh@google.com"
+var accoundPassword="123456789"
+accountCity="jaipur"
+console.log(accountId);
+accountEmail="ak@ak.com"
+accoundPassword="3nfn"
+console.table([accountId,accoundPassword,accountEmail,accountCity])
