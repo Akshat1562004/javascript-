b6e0e405aa60d3b0d47967e2764748568cc24917
Akshat1562004 <akshat.shubhit15@gmail.com>
2025-02-17 15:52:38 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as a newer version 
+let name="Akshat"
+let age=18
+let loggedIn=true
+/*
+number=>2 to power of 53
+bigint
+string
+boolean
+null
+undefined
+symbol
+*/
+console.log(typeof age ); // integer
+console.log(typeof null ); // object
+console.log(typeof undefined );//undefined
