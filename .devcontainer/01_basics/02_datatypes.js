f23fef57a79f9acab7c59b256c7aecad5b1c7682
Akshat1562004 <akshat.shubhit15@gmail.com>
2025-02-18 17:02:46 +0000

    datatypes

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -14,3 +14,22 @@ symbol
 console.log(typeof age ); // integer
 console.log(typeof null ); // object
 console.log(typeof undefined );//undefined
+/* 
+Primitive data types:
+7 types 
+BigInt,String,number,boolean,null,undefined,symbol
+Reference:
+Array,Objects,Function
+*/
+const id=Symbol('1223')
+const anotherId=Symbol('1223')
+console.log(id==anotherId);//false
+const arrayName=["Akshat","Atharv","Ritik"]//Array
+let myObj= {
+    name: "Akshat",
+    age: 22,
+}//Object
+const functions= function() {
+console.log("Hello WOrld");
+    
+}//function 
